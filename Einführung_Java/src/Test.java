c1a33dec9205aa8bad62ff1adfca7c762dcbdbeb
Julian <Julian@example.com>
2018-11-07 11:21:51 +0100

    4.4

--- Einführung_Java/src/Test.java
+++ Einführung_Java/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java 9");
+		System.out.println("Das hier ist ein Test");
 	}
 
 }
