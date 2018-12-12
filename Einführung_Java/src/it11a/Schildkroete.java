42f7d1fae37cd95180fab9c3929cf071c23aa729
sakin <sakin@cln0191.clag.local>
2018-12-12 12:21:12 +0100

    d

---
+++ Einführung_Java/src/it11a/Schildkroete.java
@@ -0,0 +1,19 @@
+package turtle;
+
+import ch.aplu.turtle.Turtle;
+
+public class Schildkroete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Turtle jack = new Turtle();
+		
+		for (int i = 1; i <= 4; i++) {
+			jack.forward(40);
+			jack.left(90);
+		} 
+
+	}
+
+}
